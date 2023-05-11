28f1b618020134571887306ef107a18b169a2d0e
youbin <you8inpark@gmail.com>
2023-05-11 16:21:41 +0900

    modify

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -14,6 +14,7 @@ const GlobalStyle = createGlobalStyle`
     }
     button {
         text-decoration: none;
+        color: ${black};
         font-family: inherit;
         border:none;
         outline: none;
